c460a283cae1a7590ae238d847e8a1cada8d2155
Kimak <mickael.dumand@gmail.com>
2017-02-24 11:51:55 +0100

    props & style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
-const todoData = ["Learn Javascript", "Learn ES6", "Learn React", "Learn React Native", "Learn Redux", "Learn Nodejs", "Learn to teach", "Teach to learn"];
+const todoData = ["Learn Javascript", "Learn ES6", "Learn React", "Learn React Native", "Learn Redux", "Learn Nodejs", "Learn by doing","Learn to teach", "Teach to learn"];
 
 const TodoItem = (props) => {
-    return (<li>{props.label}</li>);
+
+    const style = {textDecoration: "none"};
+    if(props.complete) style.textDecoration = "line-through";
+
+    return (<li style={style}>{props.label}</li>);
 }
 
 class TodoList extends Component {
@@ -11,7 +15,7 @@ class TodoList extends Component {
     return (
       <ul>{
         todoData.map((item, index)=>{
-          return <TodoItem key={index} label={item} />
+          return <TodoItem key={index} label={item} complete />
         })
       }</ul>
     );
